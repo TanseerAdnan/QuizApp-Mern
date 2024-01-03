fe2a1303b3e8d3ee05a5f62bf1f5a454bcd0acdb
Tanseer Adnan Aamer <110025105+TanseerAdnan@users.noreply.github.com>
2024-01-03 22:44:26 +0500

    Updated GameMain.jsx

--- frontend/src/game/GameMain.jsx
+++ frontend/src/game/GameMain.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const GameMain = ({ language, apiEndpoint }) => {
   const [questions, setQuestions] = useState([]);
@@ -10,6 +10,10 @@ const GameMain = ({ language, apiEndpoint }) => {
   const [percentage, setPercentage] = useState(null);
   const [showSummary, setShowSummary] = useState(false);
 
+  //To handle Game Record - for ending and saving 
+  const questionsLoaded = useRef(false);
+  const scoreRef = useRef(score);
+
   useEffect(() => {
     axios
       .get(`${apiEndpoint}/${language}Q`)
@@ -17,23 +21,24 @@ const GameMain = ({ language, apiEndpoint }) => {
         const fetchedQuestions = response.data.questions;
         const initialSelectedOptions = {};
         fetchedQuestions.forEach((_, index) => {
-          initialSelectedOptions[index] = '';
+          initialSelectedOptions[index] = "";
         });
         setSelectedOptions(initialSelectedOptions);
         setQuestions(fetchedQuestions);
+        questionsLoaded.current = true; //Questions are loaded
       })
       .catch((error) => {
-        console.error('Error fetching questions:', error);
+        console.error("Error fetching questions:", error);
       });
   }, [apiEndpoint, language]);
 
   useEffect(() => {
-    if (currentQuestionIndex >= questions.length) {
+    if (questionsLoaded.current && currentQuestionIndex >= questions.length) {
       // Game Over, save the game record and fetch the percentage
-      saveGameRecord(score);
+      saveGameRecord(scoreRef.current);
       fetchPercentage();
     }
-  }, [currentQuestionIndex, questions, score]);
+  }, [currentQuestionIndex, questions.length]);
 
   const handleOptionSelect = (option) => {
     setSelectedOptions({
@@ -45,13 +50,17 @@ const GameMain = ({ language, apiEndpoint }) => {
   const handleNextQuestion = () => {
     // Check if an option is selected
     if (!selectedOptions[currentQuestionIndex]) {
-      alert('Please choose an option before moving to the next question');
+      alert("Please choose an option before moving to the next question");
       return;
     }
 
     // Check if the selected option is correct
-    if (selectedOptions[currentQuestionIndex] === questions[currentQuestionIndex].correctAnswer) {
+    if (
+      selectedOptions[currentQuestionIndex] ===
+      questions[currentQuestionIndex].correctAnswer
+    ) {
       setScore(score + 1);
+      scoreRef.current = score + 1;
     }
 
     // Move to the next question
@@ -60,16 +69,18 @@ const GameMain = ({ language, apiEndpoint }) => {
 
   const navigate = useNavigate();
   const homePage = () => {
-    navigate('/selectedlanguage');
+    navigate("/selectedlanguage");
   };
 
   // Saving the score in DB
   const saveGameRecord = async (score) => {
     try {
-      await axios.post(`${import.meta.env.VITE_BASE_URL}/storingScore`, { score });
-      console.log('Game record saved successfully');
+      await axios.post(`${import.meta.env.VITE_BASE_URL}/storingScore`, {
+        score,
+      });
+      console.log("Game record saved successfully");
     } catch (error) {
-      console.error('Failed to save game record:', error);
+      console.error("Failed to save game record:", error);
     }
   };
 
@@ -82,13 +93,11 @@ const GameMain = ({ language, apiEndpoint }) => {
         setPercentage(fetchedPercentage);
       })
       .catch((error) => {
-        console.error('Error fetching percentage:', error);
+        console.error("Error fetching percentage:", error);
       });
   };
 
   const showSummaryHandler = () => {
-    saveGameRecord(score);
-    fetchPercentage();
     setShowSummary(true);
   };
 
@@ -98,69 +107,95 @@ const GameMain = ({ language, apiEndpoint }) => {
 
   return (
     <div className="bg-blue-500 h-screen flex flex-col justify-center items-center">
-      {currentQuestionIndex < questions.length ? (
-        <div className="bg-white p-8 rounded-lg shadow-md w-96 text-center">
-          <h2 className="text-3xl font-semibold font-sans mb-4">
-            Question {currentQuestionIndex + 1}
-          </h2>
-          <p className="mb-4 font-serif">{questions[currentQuestionIndex].question}</p>
-          <div className="flex flex-col font-serif">
-            {questions[currentQuestionIndex].options.map((option, index) => (
-              <label key={index} className="mb-2 flex items-center">
-                <input
-                  type="radio"
-                  name="options"
-                  value={option}
-                  checked={selectedOptions[currentQuestionIndex] === option}
-                  onChange={() => handleOptionSelect(option)}
-                  className="mr-2"
-                />
-                {option}
-              </label>
-            ))}
-          </div>
-          <button
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mt-4"
-            onClick={handleNextQuestion}
-          >
-            Next
-          </button>
-        </div>
-      ) : (
-        <div className="bg-gray-200 p-8 rounded-lg shadow-md w-96 text-center">
-          <h2 className="text-3xl font-semibold font-mono mb-4 shadow-lg">Game Over</h2>
-          <p>Your Score: {score}/{questions.length}</p>
-          {percentage !== null && (
-            <div className="items-center text-center rounded-lg justify-between mt-4">
-              <p className='bg-green-400 font-semibold rounded-lg'>{percentage.toFixed(2)}% Player's Scored 7 or More..</p>
+      {questions.length > 0 && (
+        <>
+          {currentQuestionIndex < questions.length ? (
+            <div className="bg-white p-8 rounded-lg shadow-md w-96 text-center">
+              <h2 className="text-3xl font-semibold font-sans mb-4">
+                Question {currentQuestionIndex + 1}
+              </h2>
+              <p className="mb-4 font-serif">
+                {questions[currentQuestionIndex].question}
+              </p>
+              <div className="flex flex-col font-serif">
+                {questions[currentQuestionIndex].options.map(
+                  (option, index) => (
+                    <label key={index} className="mb-2 flex items-center">
+                      <input
+                        type="radio"
+                        name="options"
+                        value={option}
+                        checked={
+                          selectedOptions[currentQuestionIndex] === option
+                        }
+                        onChange={() => handleOptionSelect(option)}
+                        className="mr-2"
+                      />
+                      {option}
+                    </label>
+                  )
+                )}
+              </div>
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mt-4"
+                onClick={handleNextQuestion}
+              >
+                Next
+              </button>
             </div>
-          )}
-          <div className="flex justify-between">
-            <button
-              className='bg-green-600 mt-12 py-3 px-2 rounded hover:bg-green-700 font-semibold'
-              onClick={showSummaryHandler}
-            >
-              Show Summary
-            </button>
-            <button className='bg-red-500 mt-12 py-3 px-2 rounded hover:bg-red-600 font-semibold' onClick={homePage}>
-              Play Again
-            </button>
-          </div>
-          {showSummary && (
-            <div className="summary-modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg  bg-white p-8 border-2 border-gray-300 rounded max-h-96 overflow-y-auto">
-              <button className="close-btn absolute top-4 hover:bg-red-600 right-4 bg-red-500 text-white px-4 py-2 rounded" onClick={closeSummaryHandler}>Close</button>
-              <h3 className="text-xl font-semibold underline font-sans mb-2">Summary:</h3>
-              {questions.map((question, index) => (
-                <div key={index} className="mb-2 font-serif">
-                  <p>{`Q${index + 1}: ${question.question}`}</p>
-                  <p>{`Your Answer: ${selectedOptions[index]}`}</p>
-                  <p>{`Correct Answer: ${question.correctAnswer}`}</p>
-                  <hr className="my-2" />
+          ) : (
+            <div className="bg-gray-200 p-8 rounded-lg shadow-md w-96 text-center">
+              <h2 className="text-3xl font-semibold font-mono mb-4 shadow-lg">
+                Game Over
+              </h2>
+              <p>
+                Your Score: {score}/{questions.length}
+              </p>
+              {percentage !== null && (
+                <div className="items-center text-center rounded-lg justify-between mt-4">
+                  <p className="bg-green-400 font-semibold rounded-lg">
+                    {percentage.toFixed(2)}% Player's Scored 7 or More..
+                  </p>
                 </div>
-              ))}
+              )}
+              <div className="flex justify-between">
+                <button
+                  className="bg-green-600 mt-12 py-3 px-2 rounded hover:bg-green-700 font-semibold"
+                  onClick={showSummaryHandler}
+                >
+                  Show Summary
+                </button>
+                <button
+                  className="bg-red-500 mt-12 py-3 px-2 rounded hover:bg-red-600 font-semibold"
+                  onClick={homePage}
+                >
+                  Play Again
+                </button>
+              </div>
+              {showSummary && (
+                <div className="summary-modal fixed top-1/2 left-1/2 transform -translate -x-1/2 -translate-y-1/2 rounded-lg  bg-white p-8 border-2 border-gray-300 rounded max-h-96 overflow-y-auto">
+                  <button
+                    className="close-btn absolute top-4 hover:bg-red-600 right-4 bg-red-500 text-white px-4 py-2 rounded"
+                    onClick={closeSummaryHandler}
+                  >
+                    Close
+                  </button>
+                  <h3 className="text-xl font-semibold underline font-sans mb-2">
+                    Summary:
+                  </h3>
+                  {questions.map((question, index) => (
+                    <div key={index} className="mb-2 font-serif">
+                      <p>{`Q${index + 1}: ${question.question}`}</p>
+                      <p>{`Your Answer: ${selectedOptions[index]}`}</p>
+                      <p>{`Correct Answer: ${question.correctAnswer}`}</p>
+                      <hr className="my-2" />
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           )}
-        </div>
+        </>
       )}
     </div>
   );
